0c09c773960274298350df5fcf0b520bbb523136
tetianasvidrak <svidrak.tetiana@gmail.com>
2023-06-04 11:51:23 +0200

    hw-3

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,9 +2,6 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-    if (!Object.keys(req.body).length) {
-      next(HttpError(400, "missing fields"));
-    }
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
